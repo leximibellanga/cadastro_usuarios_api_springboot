77d68b424c8826bbe8dbc6eb7d64dc3242358be0
Leximíbel Langa <langaleximibel@gmail.com>
2026-09-10 20:15:22 +0200

    style: formatacao

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,15 +1,19 @@
-
 export default function Footer() {
-    return (
-        <footer className="absolute grid place-items-center bottom-0 w-full h-20 bg-slate-900">
-            <p className="text-slate-500 text-sm">
-                &copy;{new Date().getFullYear()} Todos direitos reservados.
-                Desenvolvido por <strong>
-                    <a href="https://github.com/leximibellanga" target="_blank" className="hover:text-slate-600 hover:underline">
-                        Leximibel Langa
-                    </a>
-                </strong>
-            </p>
-        </footer>
-    )
+  return (
+    <footer className="absolute grid place-items-center bottom-0 w-full h-20 bg-slate-900">
+      <p className="text-slate-500 text-sm">
+        &copy;{new Date().getFullYear()} Todos direitos reservados. Desenvolvido
+        por{" "}
+        <strong>
+          <a
+            href="https://github.com/leximibellanga"
+            target="_blank"
+            className="hover:text-slate-600 hover:underline"
+          >
+            Leximibel Langa
+          </a>
+        </strong>
+      </p>
+    </footer>
+  );
 }
