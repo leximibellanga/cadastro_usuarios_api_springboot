b506b105fa1b9d4662bb124345faa78b63ea45e6
Leximíbel Langa <langaleximibel@gmail.com>
2026-09-10 20:15:51 +0200

    style: formatacao

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,11 +1,12 @@
-
 export default function Navbar() {
-    return (
-        <header className="flex items-center bg-slate-900 p-6 shadow-sm">
-            <nav className="container m-auto flex items-center justify-center">
-                {/* logo */}
-                <a href="#" className="font-bold text-2xl text-slate-200">API SpringBoot</a>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="flex items-center bg-slate-900 p-6 shadow-sm">
+      <nav className="container m-auto flex items-center justify-center">
+        {/* logo */}
+        <a href="#" className="font-bold text-2xl text-slate-200">
+          API SpringBoot
+        </a>
+      </nav>
+    </header>
+  );
 }
